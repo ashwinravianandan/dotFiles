d19f11c11709b4fe22cdff7303d9b1bbf0dc5908
Ashwin Ravi Anandan <ashwin.anandan@gmail.com>
2018-02-25 01:02:36 +0530

    added xmonad config

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,31 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.ManageDocks
+import System.IO
+
+myWorkspaces::[String]
+myWorkspaces = ["1:home", "2:dev", "3:web", "4", "5", "6", "7", "8", "9"]
+
+myManageHook = composeAll
+    [ className =? "Firefox"       --> doShift "3:web"
+    , className =? "Gimp"           --> doFloat
+    , className =? "Emacs"           --> doShift "2:dev"
+    ]
+
+
+main :: IO()               
+main = do
+     xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
+     xmonad $ def
+            { modMask = mod4Mask
+            , terminal = "urxvtc"
+            , logHook = dynamicLogWithPP $ xmobarPP {
+                ppOutput = hPutStrLn xmproc
+                , ppTitle = xmobarColor "green" "" . shorten 50
+                }
+            , workspaces = myWorkspaces
+            , layoutHook= avoidStruts $ layoutHook def
+            , manageHook= myManageHook
+            , handleEventHook = docksEventHook <+> handleEventHook def
+            }
